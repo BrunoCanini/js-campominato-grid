8d516600d302f361ebfd400c829d308415a39f6c
Bruno Canini <brunocanini1994@gmail.com>
2023-04-26 16:00:12 +0200

    inizo js

---
+++ main.js
@@ -0,0 +1,11 @@
+const btnPlay = document.getElementById("btnHtml");
+
+btnPlay.addEventListener("click" , function(){
+
+    for (let i = 0; i < 100; i++) {
+        const element = i;
+
+        console.log(i)
+        
+    }
+})
